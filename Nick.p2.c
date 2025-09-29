78c001ec67adddc623e7842face72f4766006b56
Nikalesh00 <medhaniklesh@gmail.com>
2025-09-29 12:58:34 +0530

    Create Nick.p2.c

---
+++ Nick.p2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int maii;
+n() {
+  int n,first=0,second=1,next,i;
+
+printf("Enter the number of terms:");
+scanf("%d",&n);
+
+for (i=0;i<n;++i) {
+if (i<=1) {
+next=i;
+} else {
+next =first+second;
+first=second;
+second=next;
+}
+printf("%d",next);
+//print comma and space if it's not the last term
+
+if (i<n-1) {
+
+printf(",");
+} 
+}
+printf("\n");
+
+return 0;
+}
+
+
